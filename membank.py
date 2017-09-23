4965ceba8191bd84f6f79f6405492d441d776db8
nemo <git@nemo.ikkoku.de>
2017-09-23 21:36:49 +0200

    Check in status quo (old; see notebook for newer)

--- membank.py
+++ membank.py
@@ -8,19 +8,30 @@ class Latch(nn.Module):
 
     def __init__(self, size, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.q = Variable(torch.zeros(size))
+        self.q_train = Variable(torch.zeros(size), requires_grad=True)
+        self.q_valid = Variable(torch.zeros(size))
         self.act = nn.Hardtanh(min_val=0, max_val=1)
         self.consent_threshold = 0.75
 
     def t(self, s):
         # all inputs in s must say 'store' or otherwise
         # there is no consent to store an value.
-        return self.act(s).mean(-1) >= self.consent_threshold
+        return (self.act(s).mean(-1) >= self.consent_threshold).float()
+
+    def q(self):
+        q = self.q_train if self.training else self.q_valid
+        return Variable(q.data)
 
     def forward(self, s, v):
-        t = self.t(s)
-        self.q = v * t + self.q * (1 - t)
-        return self.q
+        t = self.t(s).unsqueeze(-1)
+        new_q = v * t + self.q() * (1 - t)
+
+        if self.training:
+            self.q_train = new_q
+        else:
+            self.q_valid = new_q
+
+        return new_q
 
 
 class Block(nn.Module):
@@ -40,17 +51,16 @@ class Block(nn.Module):
         self.act = nn.Tanh()
 
     def forward(self, x):
-        # TODO sequential processing
         # x.shape = (b, t, u)
-        t = x.shape[1]
+        t = x.size(1)
         acts = []
         qs = []
         for ti in range(t):
-            v = self.units(x) + self.memory(self.latch.q)
+            v = self.units(x[:,ti]) + self.memory(self.latch.q())
             q = self.latch(self.set * v, self.value(v))
             acts += [self.act(v)]
             qs += [q]
-        return torch.stack(acts, dim=1), torch.stack(qs, dim=1)
+        return torch.stack(acts, dim=1), torch.stack(qs, dim=0)
 
 
 class BlockLayer(nn.Module):
@@ -62,6 +72,8 @@ class BlockLayer(nn.Module):
         self.blocks = [Block(input_dim=input_dim,
                              output_dim=self.units_per_block)
                        for _ in range(num_blocks)]
+        for i in range(len(self.blocks)):
+            self.add_module('block_{}'.format(i), self.blocks[i])
 
     def forward(self, x):
         acts = [block(x)[0] for block in self.blocks]
